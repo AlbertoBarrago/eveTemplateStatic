488d1e6101ccd3644303c2d0725ae337673f44e0
a.barrago69@gmail.com <a.barrago69@gmail.com>
2016-03-31 18:26:43 +0200

    a step back

--- src/js/main.js
+++ src/js/main.js
@@ -2,18 +2,18 @@ $('document').ready(function(){
 
 	var tl = new TimelineLite();
 
-	var logo_eve = $('#logo_eve'),
-	    title    = $('#welcome_title'),
-	    hr_custom = $('#hr_custom'),
-	    paragraph_text = $('#paragraph_text'),
-	    contact_box = $('#contact'),
-	    social_box  = $('#social');
+	var logo_eve 		= $('#logo_eve'),
+	    title    		= $('#welcome_title'),
+	    hr_custom 		= $('#hr_custom'),
+	    paragraph_text  = $('#paragraph_text'),
+	    contact_box 	= $('#contact'),
+	    social_box  	= $('#social');
  
-	tl.add( TweenLite.to(logo_eve, .5, {left:0, opacity: 1, ease: Power2.easeIn}) );
-	tl.add( TweenLite.to(welcome_title, .6, {top:0, opacity: 1, ease: Power3.easeIn}) );
-	tl.add( TweenLite.to(hr_custom, .8, {width:100, opacity:1, ease: Power1.easeIn}) );
-	tl.add( TweenLite.to(paragraph_text, .9, {opacity:1, opacity:1, ease: Power1.easeIn}) );
-	tl.add( TweenLite.to(contact_box, 1.0, {opacity:1, opacity:1, ease: Power1.easeIn}) );
-	tl.add( TweenLite.to(social_box, 1.1, {opacity:1, opacity:1, ease: Power2.easeIn}) );
+		tl.add( TweenLite.to(logo_eve, .5, {left:0, opacity: 1, ease: Power2.easeIn}) );
+		tl.add( TweenLite.to(welcome_title, .6, {top:0, opacity: 1, ease: Power3.easeIn}) );
+		tl.add( TweenLite.to(hr_custom, .8, {width:100, opacity:1, ease: Power1.easeIn}) );
+		tl.add( TweenLite.to(paragraph_text, .9, {opacity:1, opacity:1, ease: Power1.easeIn}) );
+		tl.add( TweenLite.to(contact_box, .8, {opacity:1, opacity:1, ease: Power1.easeIn}) );
+		tl.add( TweenLite.to(social_box, .85, {opacity:1, opacity:1, ease: Power2.easeIn}) );
 
 });
